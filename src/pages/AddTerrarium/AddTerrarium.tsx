ea7ac81df7c9d8595fb2ae1b5ad62e607a1291f4
Cynthia Vázquez <223205@ids.upchiapas.edu.mx>
2024-03-20 11:58:35 -0600

    AddTerrarium  modified

---
+++ src/pages/AddTerrarium/AddTerrarium.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Input } from "../../components/Input/Input";
+import  styles  from "../AddTerrarium/AddTerrarium.module.css";
+import { useLocation } from "wouter";
+
+export const AddTerrarium = () => {
+  const [, setLocation] = useLocation();
+  const handleBack = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setLocation("/");
+  };
+  return (
+    <main className={styles.main}>
+      <span>Agregar Terrarios</span>
+      <form className={styles.form}>
+        
+        <div className={styles.containerInput}>
+          <Input text="Código de registro" />
+          <Input text="Nombre" />
+          <Input text="Temperatura máxima" type="number"/>
+          <Input text="Temperatura mínima" type="number"/>
+          <Input text="Humedad máxima" type="number" />
+          <Input text="Humidad mínima" type="number" />
+          <Input text="Rayos UV máximos" type="number"/>
+          <Input text="Rayos UV mínimos" type="number"/>
+          <button className={styles.button}>Agregar</button>
+        </div>
+        <button className={styles.lastButton} onClick={handleBack}>
+          Volver
+        </button>
+      </form>
+    </main>
+  );
+};
--- src/router/IndexRouter.tsx
+++ src/router/IndexRouter.tsx
@@ -2,6 +2,7 @@ import styles from "./IndexRouter.module.css";
 import { Route } from "wouter";
 import Login from "../pages/Login/Login";
 import { Register } from "../pages/Register/Register";
+import {AddTerrarium} from "../pages/AddTerrarium/AddTerrarium";
 import { PrivateRouter } from "./privateRouter/PrivateRouter";
 
 export const IndexRouter = () => {
@@ -12,6 +13,9 @@ export const IndexRouter = () => {
       </Route>
       <Route path="/register">
         <Register />
+      </Route>
+      <Route path="/addTerrarium">
+        <AddTerrarium/>
       </Route>
       {/* añadir condicional para las rutas privadas*/}
       <PrivateRouter/> 
