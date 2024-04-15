a5f43466e0ec95f095ba73b5b30eded095b70661
Roxana Flores <rextro@MacBook-Pro-de-Roxana.local>
2024-04-14 19:25:04 -0600

    changes in ContainerMetrics

--- src/components/ContainerMetrics/ContainerMetrics.tsx
+++ src/components/ContainerMetrics/ContainerMetrics.tsx
@@ -1,21 +1,17 @@
-import React from "react";
 import styles from "./ContainerMetrics.module.css";
-import { ThemeOptions } from "../../redux/entities";
+import { ContainerImg } from "../../pages/Home/components/ContainerImg/ContainerImg";
+import { Metrics } from "../../pages/Home/components/Metrics/Metrics";
+import { InfoTerrarium } from "../../pages/Home/components/InfoTerrarium/InfoTerrarium";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/entities";
 
-interface Props {
-  theme: ThemeOptions;
-}
-
-export const ContainerMetrics: React.FC<Props> = ({ theme }) => {
+export const ContainerMetrics = () => {
+  const theme = useSelector((state: RootState) => state.theme);
   return (
-    <div
-      className={`${styles.container} ${
-        theme === "anfibios"
-          ? styles.anfibiosContainer
-          : styles.reptilesContainer
-      }`}
-    >
-      <div className={styles.metrics}>a</div>
-    </div>
+    <section className={styles.container}>
+      <ContainerImg />
+      <InfoTerrarium terrariumName={"Terrario Rex"} theme={theme} />
+      <Metrics theme={theme} />
+    </section>
   );
 };
